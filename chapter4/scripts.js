25c813ac267fe2e54c80662196dc29a317760e4b
lapeko <lapeko88@gmail.com>
2023-08-19 17:51:01 +0200

    add animation

--- chapter4/scripts.js
+++ chapter4/scripts.js
@@ -1,3 +1,21 @@
+const settings = {
+  bubbles: {
+    count: 30,
+    minSpeed: 5,
+    maxSpeed: 10,
+  },
+};
+
+const bubbleColors = [
+  { r: 255, g: 0, b: 0 },
+  { r: 0, g: 255, b: 0 },
+  { r: 0, g: 0, b: 255 },
+  { r: 255, g: 255, b: 0 },
+  { r: 255, g: 0, b: 255 },
+  { r: 0, g: 255, b: 255 },
+  { r: 255, g: 255, b: 255 },
+];
+
 const canvas = document.createElement("canvas");
 
 document.body.appendChild(canvas);
@@ -8,37 +26,46 @@ canvas.style.backgroundColor = "black";
 
 const ctx = canvas.getContext("2d");
 
+const bubbles = [];
+
 draw();
 
 function draw() {
   cleanCanvas();
-  drawBubbles(10);
+  drawBubbles();
+  requestAnimationFrame(draw);
 }
 
 function cleanCanvas() {
-  ctx.clearRect(0, 0, ctx.width, ctx.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawBubbles(count) {
-  const bubbleColors = [
-    [255, 0, 0],
-    [0, 255, 0],
-    [0, 0, 255],
-    [255, 255, 0],
-    [255, 0, 255],
-    [0, 255, 255],
-    [255, 255, 255],
-  ];
-  for (let i = 0; i < count; i++) {
-    const color = bubbleColors[Math.floor(Math.random() * bubbleColors.length)];
-    const size = Math.floor(Math.random() * 10) * 3 + 30;
-    const x = Math.floor(Math.random() * canvas.width - size);
-    const y = Math.floor(Math.random() * canvas.height - size);
-    drawBubble(x, y, size, ...color);
+function drawBubbles() {
+  if (
+    bubbles.length < settings.bubbles.count &&
+    Math.floor(Math.random() * 10) === 0
+  )
+    bubbles.push(createBubble());
+  for (let i = 0; i < bubbles.length; i++) {
+    const bubble = bubbles[i];
+    bubble.y -= bubble.speed;
+    if (bubble.y + bubble.size <= 0) bubbles[i] = createBubble();
+    drawBubble(bubble);
   }
 }
 
-function drawBubble(x, y, size, r, g, b) {
+function createBubble() {
+  const color = bubbleColors[Math.floor(Math.random() * bubbleColors.length)];
+  const size = Math.floor(Math.random() * 10) * 3 + 30;
+  const x = Math.floor(Math.random() * canvas.width);
+  const y = canvas.height + size;
+  const { minSpeed, maxSpeed } = settings.bubbles;
+  const speed =
+    Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + maxSpeed - minSpeed;
+  return { x, y, size, speed, ...color };
+}
+
+function drawBubble({ x, y, size, r, g, b }) {
   ctx.beginPath();
   const gradient = ctx.createRadialGradient(
     x - Math.round(size * 0.1),
