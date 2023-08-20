6427279d620bcb6081d3824597c7a686dea769cb
lapeko <lapeko88@gmail.com>
2023-08-20 23:38:44 +0200

    base for chapter 6

---
+++ chapter6/script.js
@@ -0,0 +1,15 @@
+// init canvas
+// add user
+// add keyboard movement for user
+// add mouse movement for user
+// add a ball
+// add ball movement
+// add collision calculation
+// add bricks
+// add brick colors
+// add brick hit logic (collision + removing + change directions of ball)
+// add score logic (you can add random * 50 to every brick)
+// draw lives and score
+// add pause, game over and restart game
+// add bonuses
+// add inc and dec ball speed
