a9dbd16f6b9280974d150abb3e9e8719bdbf6801
lapeko <lapeko88@gmail.com>
2023-08-21 23:19:52 +0200

    add a user

--- chapter6/script.js
+++ chapter6/script.js
@@ -1,4 +1,3 @@
-// add user
 // add keyboard movement for user
 // add mouse movement for user
 // add a ball
@@ -13,16 +12,62 @@
 // add bonuses
 // add inc and dec ball speed
 
-const settings = {};
+const settings = {
+  game: {
+    width: 1024,
+    height: 768,
+  },
+  user: {
+    width: 200,
+    height: 20,
+    bottom: 20,
+    color: "white",
+  },
+};
 
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 1024;
-canvas.height = 648;
+canvas.width = settings.game.width;
+canvas.height = settings.game.height;
 canvas.style.backgroundColor = "black";
 
 document.body.appendChild(canvas);
 
-const draw = () => {};
+let user;
+
+createGame();
+
+const draw = () => {
+  drawUser();
+};
 draw();
+
+function clean() {
+  ctx.clearRect(0, 0, settings.game.width, settings.game.height);
+}
+
+function drawUser() {
+  const { color, x, y, width, height } = user;
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.lineWidth = height;
+  ctx.lineCap = "round";
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + width, y);
+  ctx.stroke();
+}
+
+function createGame() {
+  const {
+    game: { width, height },
+    user: { width: uWidth, height: uHeight, bottom, color },
+  } = settings;
+  user = {
+    x: width / 2 - uWidth / 2,
+    y: height - bottom - uHeight / 2,
+    width: uWidth,
+    height: uHeight,
+    color,
+  };
+}
