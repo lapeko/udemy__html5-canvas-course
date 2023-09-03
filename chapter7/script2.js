20e83b5428ad88133e11d6f9cd810979dd79cacb
lapeko <lapeko88@gmail.com>
2023-09-03 22:38:30 +0200

    start chapter 7, example 2

--- chapter7/script2.js
+++ chapter7/script2.js
@@ -1,1 +1,8 @@
 // add jumping ball (it would be more interesting to create it with gravity)
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 1080;
+canvas.height = 768;
+canvas.style.backgroundColor = "black";
+document.body.appendChild(canvas);
